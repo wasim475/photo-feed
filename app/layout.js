36bb8db9f1055275bbfed3e775bf3144d4ab6fcf
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-27 15:14:16 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from '../components/Navbar';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <Navbar />
+        <div className="container my-4 lg:my-8">
+          {children}
+        </div>
       </body>
     </html>
   );
