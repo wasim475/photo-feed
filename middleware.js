be7eb7997de5474c94a1ffbe54bbfce11e887df7
Wasim Hossain <wasim.hossain003@gmail.com>
2025-04-09 07:50:04 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,5 +2,17 @@ import { NextResponse } from 'next/server';
 import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
-let locale = ['bn', 'en']
-let dafaultLocale = 'en'
+let locales = ['bn', 'en']
+let defaultLocale = 'en'
+
+function getLocale(request) {
+    const acceptedLanguage = request.headers.get('accept-language') ?? undefined
+    let headers = {'accept-Language': acceptedLanguage}
+    let languages = new Negotiator({headers}).languages()
+    return match(languages, locale, defaultLocale)
+}
+
+export function middleware(request){
+    const pathName = request.nextUrl.pathname
+    const pathnameIsMissingLocale = locales.every(locale=> !pathName.startsWith(`/${locale}/`) && pathName !== `/${locale}/`)
+}
