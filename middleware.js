d3df0b0a8b3eef3ed310da0095b9bdecb736f590
Wasim Hossain <wasim.hossain003@gmail.com>
2025-04-10 13:44:51 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -25,7 +25,7 @@ export function middleware(request){
 
 export const config = {
     matcher: [
-      // Skip all internal paths (_next, assets, api)
+      // Skip all internal paths (_next, assets, api)..
       '/((?!api|assets|.*\\..*|_next).*)',
       // Optional: only run on root (/) URL
       // '/'
