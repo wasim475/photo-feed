5e440cb7a019d97275f0f838bb820b9e2c4dc0ef
Wasim Hossain <wasim.hossain003@gmail.com>
2025-04-11 19:02:08 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import { getDictionary } from './[lang]/dictionaries';
+// import { getDictionary } from './[lang]/dictionaries';
 
-export default async function Home({params:{lang}}) {
-  const dictionary = await getDictionary(lang)
+export default async function Home() {
+  // const dictionary = await getDictionary(lang)
   return (
    <div>
     <h1 className='text-red-500'>hello photo feed</h1>
-    <h1 className='text-blue-500'>{dictionary.share}</h1>
+    {/* <h1 className='text-blue-500'>{dictionary.share}</h1> */}
    </div>
   );
 }
